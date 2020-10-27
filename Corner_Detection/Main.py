3328ef138b0dcb25e1395e4a775fbe9c8cff3075
ronic09 <ronic09@gmx.ch>
2020-10-27 16:35:01 +0100

    Minor efficiency improvements

--- Corner_Detection/Main.py
+++ Corner_Detection/Main.py
@@ -28,24 +28,6 @@ def main():
     descriptor_radius = 9;
     match_lambda = 4;
 
-    # Construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-ext", "--extension", required=False, default='png', help="extension name. default is 'png'.")
-    ap.add_argument("-o", "--output", required=False, default='output.mp4', help="output video file")
-    args = vars(ap.parse_args())
-
-    # Arguments
-    dir_path = 'data/'
-    ext = args['extension']
-    output = args['output']
-
-    images = []
-    for f in os.listdir(dir_path):
-        if f.endswith(ext):
-            images.append(f)
-
-    images.sort()
-
     try:
         img = Image.open('./data/000000.png')
 
@@ -75,22 +57,6 @@ def main():
     # Part 2 - Select key points and perform non-maximum surpression
     keypoints = select_key_points(harris_score, num_keypoints, nonmaximum_supression_radius)
 
-   # Determine the width and height from the first image
-    image_path = os.path.join(dir_path, images[0])
-    frame = cv2.imread(image_path)
-    cv2.imshow('video', frame)
-    height, width, channels = frame.shape
-
-    # Define the codec and create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
-    out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
-
-    image_index = 1
-
-    #for image in images:
-
-        #image_path = os.path.join(dir_path, image)
-
     # Ugly work around to convert gray scale image to RGB
     # 1. Open with CV2 with RGB codec
     # 2. Convert to PIL image
@@ -107,13 +73,8 @@ def main():
         draw.line([left, right], fill=200, width=2)
 
     frame = cv2.cvtColor(np.array(img_col), cv2.COLOR_RGB2BGR)
-    image_index = image_index + 1
-    out.write(frame)  # Write out frame to video
-
     cv2.imshow('image', frame)
     cv2.waitKey()
-#   if (cv2.waitKey(1) & 0xFF) == ord('q'):  # Hit `q` to exit
-#        break
 
     # Part 3: Build (2 * radius + 1) ^ 2 patch descriptor
     # Descriptor simply analyses intensity values of pixels
@@ -153,13 +114,42 @@ def main():
             draw.line([pix_img_1, pix_img_2], fill=(124, 252, 0), width=2)
 
     frame = cv2.cvtColor(np.array(img_col_2), cv2.COLOR_RGB2BGR)
-    #image_index = image_index + 1
-    out.write(frame)  # Write out frame to video
-
     cv2.imshow('image', frame)
     cv2.waitKey()
 
     # Part 5: Run corner detection algorithm for all images
+
+    # Construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-ext", "--extension", required=False, default='png', help="extension name. default is 'png'.")
+    ap.add_argument("-o", "--output", required=False, default='output.mp4', help="output video file")
+    args = vars(ap.parse_args())
+
+    # Arguments
+    dir_path = 'data/'
+    ext = args['extension']
+    output = args['output']
+
+    images = []
+    for f in os.listdir(dir_path):
+        if f.endswith(ext):
+            images.append(f)
+
+    images.sort()
+
+    # Determine the width and height from the first image
+    image_path = os.path.join(dir_path, images[0])
+    frame = cv2.imread(image_path)
+    cv2.imshow('video', frame)
+    height, width, channels = frame.shape
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
+    out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
+
+    image_index = 1
+
+    #for image in images:
 
 
 
